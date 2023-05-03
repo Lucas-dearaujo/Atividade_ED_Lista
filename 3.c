cdad78583a322516af20976c53763f173ccf6f93
Lucas de Araujo Ferreira Maia <131637819+Lucas-dearaujo@users.noreply.github.com>
2023-05-03 16:26:50 -0300

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,57 +1,110 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct pilha_A
-{
-    int caixa;
-    struct pilha_A * prox;
-}A;
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct no{
+    int peso;
+    struct no * prox;
+}NO;
+typedef struct pilha{
+    struct no * topo;
+    int tam;
+}PILHA;
 
-int Tamanho_A=0;
-A * topo_a= NULL;
-
-void recursao(A * topo, A * caixa_nova){
-    if(caixa_nova->caixa <= topo->caixa || topo->prox == NULL ){
-      topo->prox=caixa_nova;
-      topo_a=topo;
-    }
-    else{
-        recursao(topo->prox, caixa_nova);
-        topo->prox=topo_a;
-        topo_a=topo;
+PILHA * inicia(){
+    PILHA * p = malloc(sizeof(PILHA));
+    p->tam = 0;
+    p->topo = NULL;
+    return p;
+}
+void add(PILHA * pA, int peso){
+    NO * novo = malloc(sizeof(NO));
+    novo->prox = NULL;
+    novo->peso = peso;
+        if(pA->topo == NULL){
+            pA->topo = novo;
+        }
+        else if(pA->topo->peso < peso){
+            PILHA * pC = inicia();
+            PILHA * pB = inicia();
+            while(pA->topo != NULL){
+                if(pA->topo->peso == novo->peso){
+                    novo->prox = pA->topo;
+                    pA->topo= novo;
+                    break;
+                }
+                if(pA->topo->peso==3){
+                   if(pC->topo == NULL){
+                        pC->topo = pA->topo;
+                        pA->topo = pA->topo->prox;
+                        pC->topo->prox = NULL;
+                   }
+                   else if(pC->tam > 0){
+                       NO * aux = pA->topo->prox;
+                       pA->topo->prox = pC->topo;
+                       pC->topo = pA->topo;
+                       pA->topo = aux;
+                   }
+                pA->tam--;
+                pC->tam++;
+                }
+                else if(pA->topo->peso==5){
+                   if(pB->topo == NULL){
+                        pB->topo = pA->topo;
+                        pA->topo = pA->topo->prox;
+                        pB->topo->prox = NULL;
+                   }
+                    else if(pB->tam > 0){
+                        NO * aux = pA->topo->prox;
+                        pA->topo->prox = pB->topo;
+                        pB->topo = pA->topo;
+                        pA->topo = aux;
+                    }
+                pA->tam--;
+                pB->tam++;
+                }
+            }
+            if(pA->topo == NULL){
+                pA->topo = novo;
+            }
+            while(pB->tam > 0 && pC->tam > 0){
+                if(pA->topo->peso >= 5){
+                    NO * aux = pB->topo->prox;
+                    pB->topo->prox = pA->topo;
+                    pA->topo = pB->topo;
+                    pB->topo = aux;
+                    pB->tam--;
+                    pA->tam++;
+                }
+                if(pA->topo->peso >=3 && pB->tam == 0){
+                    NO * aux = pC->topo->prox;
+                    pC->topo->prox = pA->topo;
+                    pA->topo = pC->topo;
+                    pC->topo = aux;
+                    pC->tam--;
+                    pA->tam++;   
+                }
+            }
+        }
+        else{
+            novo->prox = pA->topo;
+            pA->topo = novo;
+        }
+        pA->tam++;
+}
+void imprimir(PILHA * p){
+    NO * aux = p->topo;
+    while(aux != NULL){
+        printf("%d\n" , aux->peso);
+        aux = aux->prox;
     }
 }
-
-void Add_in_Pilha_main(int caixa){
-    A * novo = malloc(sizeof(A));
-    novo->prox=NULL;
-    novo->caixa=caixa;
-    
-    if(Tamanho_A == 0){
-        topo_a=novo;
-    }
-    else if (Tamanho_A > 0 && novo->caixa > topo_a->caixa){
-        recursao(topo_a, novo);
-    }
-    else{
-        novo->prox=topo_a;
-        topo_a=novo;
-    }
-    Tamanho_A++;
-}
-
-void imprimir(){
-    A * aux=topo_a;
-    while (aux != NULL){
-       printf("Caixa:%d\n",aux->caixa);
-       aux=aux->prox;
-    }
-    
-}
-
-int main(){
-  Add_in_Pilha_main(5);
-  Add_in_Pilha_main(7);
-  Add_in_Pilha_main(5);
-  Add_in_Pilha_main(7);
-  imprimir();
+int main() {
+    PILHA * pA = inicia();
+    add(pA, 7);
+    add(pA, 5);
+    add(pA, 5);
+    add(pA, 3);
+    add(pA, 7);
+    printf("\n\n");
+    imprimir(pA);
+    printf("Tamanho de pA :%d" , pA->tam);
 }
