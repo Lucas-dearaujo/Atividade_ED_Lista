d43e0d4ea7551ced658d35e65916ebe3dfe994f3
Lucas de Araujo Ferreira Maia <131637819+Lucas-dearaujo@users.noreply.github.com>
2023-05-08 08:51:23 -0300

    Satisfação.c
    agora foi depois de muita peia, mesmo com a tatiane falando como fazer foi chao.

--- 5.c
+++ 5.c
@@ -1,4 +1,4 @@
-// Online C compiler to run C program online
+
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct no{
@@ -10,16 +10,24 @@ typedef struct pilha{
     int tam;
 }PILHA;
 
-void add(char n , PILHA * p){
-NO * novo = malloc(sizeof(NO));
-novo->prox= NULL;
-novo->n= n;
-
+void desempilha(PILHA * p){
+        NO * aux = p->topo;
+        NO * lixo = p->topo;
+            for(int i = p->tam; i > 0; i--){
+                printf("%c", aux->n);
+                aux = aux->prox;
+                free(lixo);
+                lixo = aux;
+                p->tam--;
+            }
+            p->topo = NULL;
+}
+void empilha(PILHA * p , char n){
+    NO * novo = malloc(sizeof(NO));
+    novo->prox= NULL;
+    novo->n= n;  
     if(p->topo == NULL){
         p->topo = novo;
-        if(novo->n == 'a' || novo->n == 'e' || novo->n == 'i' || novo->n == 'o' || novo->n == 'u'){
-           printf("%c", novo->n); 
-        }
     }
     else if(novo->n != 'a' && novo->n != 'e' && novo->n != 'i' && novo->n != 'o' && novo->n != 'u'){
         novo->prox = p->topo;
@@ -36,24 +44,38 @@ novo->n= n;
                 lixo = aux;
                 p->tam--;
             }
-            printf("%c", novo->n);
+            printf("%c" , novo->n);
             free(novo);
-            p->topo = NULL;
+            
     }
     p->tam++;
 }
+
 int main(){
     char temp;
+    
+    //1. Inicia a pilha na main
     PILHA * p = malloc(sizeof(PILHA));
     p->topo = NULL;
     p->tam = 0;
     printf("Digita ai\n"); 
+  
+  //2. recebe caracteres ate dar enter.
   do
   {
     temp = getchar();
-    if(temp != '\n'){
-        add(temp, p);
+    if(temp == '\n'){
+    desempilha(p);
+    }
+    if(temp != 'a' && temp != 'e' && temp != 'i' && temp != 'o' && temp != 'u'){
+        empilha(p, temp);
+    }
+    else if(temp == 'a' && temp == 'e' && temp == 'i' && temp == 'o' && temp == 'u'){
+        desempilha(p);
+        printf("%c", temp);
     }
   }while(temp != '\n');
  
-}
+}   
+        //Testes: acbda = adbca
+        //bacagdf = bacafdg   
